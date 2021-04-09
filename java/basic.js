0ef86c0d67019fae913750dd431a58bb05860a99
Branka Kooij <Branka.Kooij@gmail.com>
2021-04-09 18:50:46 +0200

    correct

---
+++ java/basic.js
@@ -0,0 +1,19 @@
+console.log('Hello Winc Academy');
+   let firstName = 'branka';
+   console.log(firstName);
+
+   let x = 4;
+   x = 5 ;
+   let y = 4;
+   let z= x+y;
+   console.log(z);
+const a = 15;
+let b = 75;
+let c = a / b;
+console.log(c);
+let frName = "Niek";
+let lastName= "Kooij";
+let  fullName = frName + lastName;
+let age = "48";
+console.log(fullName);
+console.log(typeof age);
