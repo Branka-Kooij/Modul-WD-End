fe1faf3e5c9802b0f4f127e893fff745d23eceb9
Branka Kooij <Branka.Kooij@gmail.com>
2021-04-10 19:59:24 +0200

    Create scriptCO.js

---
+++ comparison -operations/scriptCO.js
@@ -0,0 +1,38 @@
+let age = 50;
+if(age>18 && age<25)
+{
+    console.log("Je krijgt 50% korting!")
+}
+else { 
+    console.log("Sorry geen 50% korting!")
+}
+
+const firsttName = "Sara" ;
+if (firsttName=="Sara" || firsttName == "Abraham")
+{
+    console.log("Je krijgt 20% korting!");
+}
+else {
+    console.log("Sorry Geen korting!");
+}
+
+
+const totalAmount = 3;
+if (totalAmount >= 100 )
+{
+    console.log(" Dan krijg je een gratis flesje champagne!");
+
+}
+else if (totalAmount >50)
+{
+    console.log("Dan krijg je gratis portie nachos.!");
+}
+else if ( totalAmount > 25)
+{
+    console.log("Dan krijg je gratis (vega)bitterballen");
+
+}
+else {
+    console.log("sorry");
+
+}
