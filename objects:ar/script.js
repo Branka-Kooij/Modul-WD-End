b355f7e3ef255ac905b306787d0de2acfae9adc3
Branka Kooij <Branka.Kooij@gmail.com>
2021-04-11 17:28:33 +0200

    q

---
+++ objects:ar/script.js
@@ -0,0 +1,24 @@
+let person = {
+    name: 'Branka',
+    age : 38,
+
+};
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+
+ let evaluations = [7,10,9];
+ console.log(evaluations);
+
+
+
+//deel 2
+
+
+let array =[ 'green','red', 'blue'];
+array.push('yellow');
+array.push('5');
+array.push({ greeting :'Ik ben een object'});
+console.log(array);
+console.log (array.length);
+console.log (array [3]);
