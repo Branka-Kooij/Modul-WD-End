0dd11da0662b1440e97f248ab7378c158d928b3c
Branka Kooij <Branka.Kooij@gmail.com>
2021-04-12 22:35:48 +0200

    Create agrument.js

---
+++ argukment/agrument.js
@@ -0,0 +1,21 @@
+
+
+function paint( color) {
+    console.log('The wall has been painted' + color );
+   
+
+
+};
+paint( 'green');
+
+
+function paint( color, wallTyple) {
+    console.log('The '+  wallTyple+'wall has been painted' + color );
+   
+
+
+};
+paint( 'orange' , ' north ');
+
+
+paint( 'gray' , ' south');
