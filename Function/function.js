be99043de562c05a7f3d8ad61d688673ed5e4c8e
Branka Kooij <Branka.Kooij@gmail.com>
2021-04-12 19:56:21 +0200

    Create function.js

---
+++ Function/function.js
@@ -0,0 +1,29 @@
+
+function mmakingSushi ()
+{
+
+    console.log(" Cook rice in water");
+    console.log("Mix sugar and vinegar. Combine it with cooled rice ");
+    console.log("Put in the center filling");
+    console.log(" Chop the roll into slices ");
+}
+mmakingSushi();
+mmakingSushi();
+mmakingSushi();
+
+
+
+
+
+function  makingPizza( ){
+    console.log("Make a dough");
+    console.log("Pre-heat oven to 250 degrees C");
+    console.log("Put on the pizza sauce, cheese, and toppings");
+    console.log("Put the pizza  in the oven.");
+
+
+}
+
+
+makingPizza();
+makingPizza();
