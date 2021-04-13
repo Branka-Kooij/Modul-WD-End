a4259bb77211d7c7c7ae4cc62e5448436ca81d51
Branka Kooij <Branka.Kooij@gmail.com>
2021-04-13 21:33:59 +0200

    Create return.js

---
+++ return/return.js
@@ -0,0 +1,48 @@
+
+
+/*
+function number (){
+  
+  if (number>100){
+
+  
+    return true;
+  }
+   return false;
+};
+
+let resault =number(90);
+console.log(resault);
+*/
+/*
+function bouncerBrenda  (maxpeople , _visitorsAge , currentnumber){
+    if  (currentnumber > maxpeople ){
+    return 'it is too busy';
+}
+
+if (_visitorsAge <18){
+    return "This is a club for adults";
+}
+else {
+    return "Come in";
+}
+};
+
+
+console.log(bouncerBrenda(100, 17, 50));
+console.log(bouncerBrenda(100, 19, 101));
+console.log(bouncerBrenda(100, 15, 75));
+console.log(bouncerBrenda(100, 35, 103));
+
+
+*/
+
+
+function averageNumber ( num1 , num2, num3, num4, num5)
+{
+sum = num1 + num2+num3+ num4+num5;
+average= sum /5
+resaultAvarage=Math.round(average);
+  return (resaultAvarage);
+};
+console.log(averageNumber(1,1,2,7,1,));
