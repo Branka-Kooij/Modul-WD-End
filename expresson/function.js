46cbb3a1e6530a1326cc6df4e4433014d5a9797c
Branka Kooij <Branka.Kooij@gmail.com>
2021-04-14 11:16:46 +0200

    chaqge

--- expresson/function.js
+++ expresson/function.js
@@ -3,14 +3,14 @@ function squarDeclaration (num1, num2){
     const square2 = num2 * num2;
     const sum  = square1 + square2;
     const sumSquare = sum * sum;
-  return sumSquare;
+return sumSquare;
 }
 console.log(squarDeclaration(1,2))
 
 
 const squareExpresion = function ( num1, num2)
 {
-       const square1 = num1 * num1;
+      const square1 = num1 * num1;
       const square2 = num2 * num2;
       const sum  = square1 + square2; 
       const sumSquare = sum * sum;
@@ -28,10 +28,8 @@ const squareArrow = (num1, num2)=>
     const square1 = num1 * num1;
      const square2 = num2 * num2;
      const sum= square1 + square2;
-
      const sumSquare = sum * sum;
-
-     return sumSquare;
+return sumSquare;
 
 };
 
