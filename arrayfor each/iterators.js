a4f1dda500d4f03d4cf12cdf7c28aa716d93edf8
Branka Kooij <Branka.Kooij@gmail.com>
2021-04-15 20:01:20 +0200

    Create iterators.js

---
+++ arrayfor each/iterators.js
@@ -0,0 +1,26 @@
+const arreyColor =['green' , 'blue' , "red ",  " orange" ];
+let i=0;
+while(i <arreyColor.length) {
+console.log(arreyColor[i]);
+i++
+}
+
+for (let i=0; i<arreyColor; i++){
+    console.log(arreyColor[i]);
+}
+//NEW
+
+ arreyColor.forEach(color => console.log(color))
+
+
+ 
+ let colorOBject = {
+    colorY: 'yellow',
+    colorS: 'blue',
+    colorF: 'red',
+    colorL: 'orange'
+};
+
+for (i in colorOBject) {
+    console.log(colorOBject[i]);
+}
